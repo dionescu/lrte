e6bd2224034061cf56fb110574508806a8839ebd
dionescu <dionescu88@gmail.com>
2016-12-03 23:22:22 -0500

    More cosmetic changes to the crosstool

--- crosstool/get-gcc-svn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Download gcc code from svn.
-SVN_TOP=${SVN_TOP:-sources}
-BRANCH=branches/google/gcc-4_9
-cd ${SVN_TOP}
-svn co svn://gcc.gnu.org/svn/gcc/${BRANCH}
-cd -
--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 absname=`readlink -f "$0"`
 absroot="$(dirname $absname)"
-cfg="${absroot}/grte.cfg"
+cfg="${absroot}/grte/grte.cfg"
 
 [ -f $cfg ] || {
     echo "could not find $cfg"
@@ -28,3 +28,14 @@ wget https://ftp.gnu.org/gnu/mpfr/mpfr-${mpfr_version}.tar.bz2
 wget https://ftp.gnu.org/gnu/mpc/mpc-${mpc_version}.tar.gz
 wget http://zlib.net/zlib-${zlib_version}.tar.gz
 wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-${headers26}.tar.xz
+
+GCC_BRANCH=branches/google/gcc-4_9
+svn co svn://gcc.gnu.org/svn/gcc/${GCC_BRANCH}
+
+CLANG_BRANCH=branches/google/stable
+svn co http://llvm.org/svn/llvm-project/llvm/${CLANG_BRANCH} llvm
+svn co http://llvm.org/svn/llvm-project/compiler-rt/${CLANG_BRANCH} llvm/projects/compiler-rt
+svn co http://llvm.org/svn/llvm-project/cfe/${CLANG_BRANCH} llvm/tools/clang
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/${CLANG_BRANCH} llvm/tools/clang/tools/extra
+
+
